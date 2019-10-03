e4ff358ebd2deaf1ab5eae6afb3be850a57c297d
pattoche12 <patange612@gmail.com>
2019-10-03 16:18:29 +0200

    final modif interfaces

--- Eagle.java
+++ Eagle.java
@@ -1,24 +1,83 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly
+{
 
     private boolean flying;
     private int altitude;
 
-    public Eagle(String name) {
-        super(name);
-        this.flying = false;
-        this.altitude = 0;
-    }
+    	public Eagle(String name) 
+	{
+		super(name);
+		this.flying = false;
+		this.altitude = 0;
+    	}
 
-    public int getAltitude() {
-        return altitude;
-    }
+    	public int getAltitude() 
+	{
+        	return altitude;
+    	}
 
-    public boolean isFlying() {
-        return flying;
-    }
+    	public boolean isFlying() 
+	{
+        	return flying;
+    	}
 
-    @Override
-    public String sing() {
-        return "Screech!";
-    }
+    	@Override
+   	public String sing() 
+	{
+        	return "Screech!";
+    	}
+
+ 	
+   	public void takeOff() 
+	{
+		if (!this.flying && this.altitude == 0) {
+			this.flying = true;
+			System.out.printf("%s takes off in the sky.%n", this.getName());
+		}
+	}
+
+
+ 
+   	@Override
+    	public int ascend(int meters) 
+	{
+		if (this.flying) 
+		{
+		    this.altitude = Math.min(this.altitude + meters, 325);
+		    System.out.printf("%s cherry flies upward, altitude : %d%n", this.getName(), this.altitude);
+       	 	}
+       		return this.altitude;
+    	}
+
+    
+    	@Override
+    	public int descend(int meters) 
+	{
+		if (this.flying) 
+		{
+		    this.altitude = Math.max(this.altitude - meters, 0);
+		    System.out.printf("%s flies downward, altitude : %d%n", this.getName(), this.altitude);
+		}
+        	return this.altitude;
+    	}
+
+    
+    	@Override
+    	public void land() 
+	{
+		if (this.altitude > 1) 
+		{
+		    System.out.printf(this.getName() + "is to high, it can't land. ");
+		}
+		else 
+		{
+		    System.out.printf(this.getName() + "lands on the ground. ");
+		}
+	}
+   	@Override
+	public void glide() 
+	{
+		Fly.super.glide();
+	}
 }
+
